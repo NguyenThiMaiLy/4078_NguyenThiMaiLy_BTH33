f9bc41b11f0ca5c92a1175c654518dd6ef93643f
Administrator <Administrator@10PC327>
2024-03-22 16:21:27 +0700

    Add project files.

---
+++ 4078_NguyenThiMaiLy_BTH3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _4078_NguyenThiMaiLy_BTH3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
